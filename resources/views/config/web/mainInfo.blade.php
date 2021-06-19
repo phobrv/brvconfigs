3ca34f7cf8948df743bb743300f22613dd1ca191
MrPho <nguyentrongpho@gmail.com>
2021-06-19 23:55:48 +0700

    update

--- resources/views/config/web/mainInfo.blade.php
+++ resources/views/config/web/mainInfo.blade.php
@@ -3,16 +3,13 @@
 		<input type="hidden" name="type" value="web">
 		@csrf
 		<div class="box-body">
-			<label class="font16">Contact</label>
+			@include('phobrv::input.label',['label'=>'Contact'])
 			@include('phobrv::input.inputText',['label'=>'Hotline number','key'=>'hotline_number','type'=>'configs'])
 			@include('phobrv::input.inputText',['label'=>'Email','key'=>'company_email','type'=>'configs'])
-			<hr class="border-light">
-			<label class="font16">Info</label>
+			@include('phobrv::input.label',['label'=>'Info'])
 			@include('phobrv::input.inputText',['label'=>'Tên công ty','key'=>'company_name','type'=>'configs'])
 			@include('phobrv::input.inputText',['label'=>'Address','key'=>'company_add','type'=>'configs'])
-
-			<hr class="border-light">
-			<label  class="font16">Copyright</label>
+			@include('phobrv::input.label',['label'=>'Copyright'])
 			@include('phobrv::input.inputText',['label'=>'Copyright','key'=>'copyright','type'=>'configs'])
 			<div class="box-footer">
 				<button type="submit" class="btn btn-primary pull-right">{{__('Submit')}}</button>
--- resources/views/config/web/social.blade.php
+++ resources/views/config/web/social.blade.php
@@ -3,11 +3,11 @@
 		<input type="hidden" name="type" value="web">
 		@csrf
 		<div class="box-body">
-			<lable class="font16">Facebook</lable>
+			@include('phobrv::input.label',['label'=>'Facebook'])
 			@include('phobrv::input.inputText',['label'=>'Fb app_id','key'=>'fb_app_id','type'=>'configs'])
 			@include('phobrv::input.inputText',['label'=>'Fanpage','key'=>'company_fb','type'=>'configs'])
 			@include('phobrv::input.inputTextarea',['label'=>'Code Fanpage','key'=>'code_fanpage','type'=>'configs'])
-			<lable class="font16">Other</lable>
+			@include('phobrv::input.label',['label'=>'Other'])
 			@include('phobrv::input.inputText',['label'=>'Youtube','key'=>'company_youtube','type'=>'configs'])
 			@include('phobrv::input.inputText',['label'=>'Twitter','key'=>'company_twitter','type'=>'configs'])
 			@include('phobrv::input.inputText',['label'=>'Printer','key'=>'company_printer','type'=>'configs'])
